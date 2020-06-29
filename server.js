65c31f0b47c9e482f63a2c1ae8966152e6a665cf
FedBashilov <fedbashilov@gmail.com>
2020-06-29 04:10:08 +0300

    fix CORS

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+
+const whitelist = ['https://rocky-caverns-04393.herokuapp.com']; // allow domain
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (whitelist.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
+
+app.use(cors(corsOptions));
 
 app.use(express.static(path.join(__dirname, '/dist/Todos')));
 app.listen(process.env.PORT || 8080);
